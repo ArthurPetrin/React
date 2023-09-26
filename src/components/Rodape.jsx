80f31aff1752831ebfea6ee0ac1e511ea06ced69
Arthur Petrin <arthurpetrin05@gmail.com>
2023-09-26 11:48:26 -0300

    coisa

---
+++ src/components/Rodape.jsx
@@ -0,0 +1,13 @@
+import {} from 'react'
+import { FaBeer } from 'react-icons/fa';
+import styles from "../css/Rodape.module.css"
+
+function Ropade (){
+  return(
+    <section className={styles.rodape}>
+    <h3>@2023-todos os direitos reservados</h3>
+    <FaBeer/>
+        </section>
+  )
+}
+export default Ropade
